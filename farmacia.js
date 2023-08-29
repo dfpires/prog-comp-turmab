2f37c77b1e6d3ea8c02cd0f34811083902e93918
Daniel Pires <dfpires@gmail.com>
2023-08-29 20:27:27 -0300

    Add files via upload

--- farmacia.js
+++ farmacia.js
@@ -1,12 +1,15 @@
-function principal(){
+// iremos tornar nossas funções em variáveis
+principal() // chama a função principal
+// a minha variável principal é uma função
+let principal = () => {
     let vetFarmacias = []
     let vetRemedios = []
     cadastraFarmacias(vetFarmacias)
     cadastraRemedios(vetRemedios, vetFarmacias)
-//    vendaRemedios(vetRemedios)
+    vendaRemedios(vetRemedios)
 }
 
-function cadastraFarmacias(vetFarmacias){
+let cadastraFarmacias = (vetFarmacias) => {
     for(let i=0;i<5;i++){
         let novaFarmacia = {
             codigo: Number(prompt('Código da farmacia')),
@@ -22,7 +25,7 @@ function cadastraFarmacias(vetFarmacias){
     }
 }
 
-function cadastraRemedios(vetRemedios, vetFarmacias){
+let cadastraRemedios = (vetRemedios, vetFarmacias) => {
     for(let i=0;i<5;i++){
         let novoRemedio = {
             codigoFarmacia: Number(prompt('Código da farmácia')),
@@ -36,4 +39,34 @@ function cadastraRemedios(vetRemedios, vetFarmacias){
         }
         vetRemedios.push(novoRemedio)
     }
+}
+
+let vendaRemedios = (vetRemedios) => {
+    for(let cont=0;cont<5;cont++){
+        let objVenda = {
+            codigo: Number(prompt('Código do remédio')),
+            nome: prompt('Nome do remédio'),
+            qtde: Number(prompt('Qtde vendida')),
+        }
+        // percorre o vetor vetRemedios procurando pelo código 
+        // do remédio, e o nome do remédio
+        let achou = false // não encontrei o remédio
+        for(let i=0;i<vetRemedios.length;i++){
+            if (vetRemedios[i].codigoFarmacia == objVenda.codigo 
+                && vetRemedios[i].nome == objVenda.nome){
+                achou = true // achei o remédio
+                // vamos realizar a venda
+                if (vetRemedios[i].qtde >= objVenda.qtde){
+                    vetRemedios[i].qtde = vetRemedios[i].qtde - objVenda.qtde
+                }
+                else {
+                    alert('Estoque insuficiente para esta venda')
+                }
+            }
+        }
+        if(!achou){
+            alert('Produto não encontrado')
+        }
+    }
+
 }
